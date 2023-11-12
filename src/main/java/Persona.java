523a67638e8bada8d93bb75fb360f7fc0b15c5d7
TGO74 <t.gutierrez04@ufromail.cl>
2023-11-11 23:07:56 -0300

    modificaciones 2 ayudantía 7

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,5 +1,4 @@
 
-import java.util.ArrayList;
 public class Persona {
     protected String nombre;
     protected String apellido;
@@ -47,6 +46,71 @@ public class Persona {
     //Método para cambiar direccion
     public void cambiarDireccion(String direccion) {
         this.direccion = direccion;
+    }
+
+//Método para cambiar edad
+    public void cambiarEdad(int edad) {
+        this.edad = edad;
+    }
+
+    //Método para cambiar nombre
+    public void cambiarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Método para cambiar apellido
+    public void cambiarApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    //Método para cambiar rut
+    public void cambiarRut(String rut) {
+        this.rut = rut;
+    }
+
+    //Método para obtener numero telefonico
+    public String obtenerNumeroTelefonico() {
+        return this.numeroTelefonico;
+    }
+
+    //Método para obtener direccion
+    public String obtenerDireccion() {
+        return this.direccion;
+    }
+
+    //Método para obtener edad
+    public int obtenerEdad() {
+        return this.edad;
+    }
+
+    //Método para obtener nombre
+    public String obtenerNombre() {
+        return this.nombre;
+    }
+
+    //Método para obtener apellido
+    public String obtenerApellido() {
+        return this.apellido;
+    }
+
+    //Método para obtener rut
+    public String obtenerRut() {
+        return this.rut;
+    }
+
+    //Método para obtener nombre completo
+    public String obtenerNombreCompleto() {
+        return this.nombre + " " + this.apellido;
+    }
+
+    //Método para obtener datos
+    public String obtenerDatos() {
+        return "Nombre: " + this.nombre + "\n" + "Apellido: " + this.apellido + "\n" + "Rut: " + this.rut + "\n" + "Edad: " + this.edad + "\n" + "Dirección: " + this.direccion + "\n" + "Número telefónico: " + this.numeroTelefonico;
+    }
+
+    //Método para obtener datos de persona
+    public String obtenerDatosPersona() {
+        return "Nombre: " + this.nombre + "\n" + "Apellido: " + this.apellido + "\n" + "Rut: " + this.rut + "\n" + "Edad: " + this.edad + "\n" + "Dirección: " + this.direccion + "\n" + "Número telefónico: " + this.numeroTelefonico;
     }
 
 
